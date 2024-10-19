e6fdce098c631847843fe58d7c49921d8e798b64
Singh Pritesh <singhpritesh7599@gmail.com>
2024-10-19 14:18:34 +0530

    updating without authentication view

--- client/src/components/common/check-auth.jsx
+++ client/src/components/common/check-auth.jsx
@@ -6,56 +6,68 @@ function CheckAuth({ isAuthenticated, user, children }) {
 
     const location = useLocation();
 
-    if(location.pathname === "/"){
-        if(!isAuthenticated){
-            return <Navigate to="/auth/login" />;
-        } else {
-            if(user?.role === "admin") {
-                return <Navigate to="/admin/dashboard" />;
-            } else {
-                return <Navigate to="/shop/home" />;
-            }
+
+
+    if (location.pathname === "/shop/checkout" || location.pathname === "/shop/account") {
+        if (!isAuthenticated) {
+            return <Navigate to="/auth/login" />
         }
     }
 
-
-    if (
-        !isAuthenticated &&
-        !(
-            location.pathname.includes("/login") ||
-            location.pathname.includes("/register")
-        )
-    ) {
-        return <Navigate to="/auth/login" />;
+    if (location.pathname === "/") {
+        if (!isAuthenticated) {
+            return <Navigate to="/shop/home" />
+        }
+        // if (!isAuthenticated) {
+        //     return <Navigate to="/auth/login" />;
+        // } 
+        else {
+             if (user?.role === "admin") {
+                 return <Navigate to="/admin/dashboard" />;
+             } else {
+                 return <Navigate to="/shop/home" />;
+             }
+         }
     }
+
+
+    // if (
+    //     !isAuthenticated &&
+    //     !(
+    //         location.pathname.includes("/login") ||
+    //         location.pathname.includes("/register")
+    //     )
+    // ) {
+    //     return <Navigate to="/auth/login" />;
+    // }
 
     if (
         isAuthenticated &&
         (location.pathname.includes("/login") ||
             location.pathname.includes("/register"))
     ) {
-        if(user?.role === "admin") {
+        if (user?.role === "admin") {
             return <Navigate to="/admin/dashboard" />;
         } else {
             return <Navigate to="/shop/home" />;
         }
     }
 
-    if (
-        isAuthenticated &&
-        user?.role !== "admin" &&
-        location.pathname.includes("/admin")
-    ) {
-        return <Navigate to="/unauth-page" />;
-    }
+    // if (
+    //     isAuthenticated &&
+    //     user?.role !== "admin" &&
+    //     location.pathname.includes("/admin")
+    // ) {
+    //     return <Navigate to="/unauth-page" />;
+    // }
 
-    if (
-        isAuthenticated &&
-        user?.role === "admin" &&
-        location.pathname.includes("shop")
-    ) {
-        return <Navigate to="/admin/dashboard" />;
-    }
+    // if (
+    //     isAuthenticated &&
+    //     user?.role === "admin" &&
+    //     location.pathname.includes("shop")
+    // ) {
+    //     return <Navigate to="/admin/dashboard" />;
+    // }
 
     return <>{children}</>;
 }
